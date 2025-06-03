84d53de4bf84b8da486a36dbd07b55eb92115ef1
dvsmc <112554734+dvsmc@users.noreply.github.com>
2025-06-03 15:55:46 +0300

    0.5.2 2

--- src/parser.c
+++ src/parser.c
@@ -3495,7 +3495,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_mdsl(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 5,
-      .patch_version = 1,
+      .patch_version = 2,
     },
   };
   return &language;
